0e543e0473d9344650bf5676bdc24f7f6ecc8e98
Rajanish GJ <rajanishgj@google.com>
2017-06-30 14:04:01 -0700

    [XAPID-999] fix duplicate config in response, added configuration to provide configBundleBlobDownloadEndpoint

--- data.go
+++ data.go
@@ -140,24 +140,48 @@ func (dbc *dbManager) getUnreadyBlobs() (ids []string, err error) {
 // TODO there's a bug in the db statement
 func (dbc *dbManager) getReadyDeployments() ([]DataDeployment, error) {
 
-	rows, err := dbc.getDb().Query(`SELECT
-	a.id,
-	a.organization_id,
-	a.environment_id,
-	a.bean_blob_id,
-	a.resource_blob_id,
-	a.type,
-	a.name,
-	a.revision,
-	a.path,
-	a.created_at,
-	a.created_by,
-	a.updated_at,
-	a.updated_by,
-	b.local_fs_location
-	FROM metadata_runtime_entity_metadata as a
-	INNER JOIN edgex_blob_available as b
-	ON (a.bean_blob_id = b.id OR a.resource_blob_id = b.id)
+	rows, err := dbc.getDb().Query(`
+		SELECT 	a.id,
+			a.organization_id,
+			a.environment_id,
+			a.bean_blob_id,
+			a.resource_blob_id,
+			a.type,
+			a.name,
+			a.revision,
+			a.path,
+			a.created_at,
+			a.created_by,
+			a.updated_at,
+			a.updated_by,
+			b.local_fs_location
+		FROM metadata_runtime_entity_metadata as a
+		INNER JOIN edgex_blob_available as b
+		ON (a.bean_blob_id = b.id)
+		WHERE a.id IN (
+			SELECT
+					a.id
+				FROM metadata_runtime_entity_metadata as a
+				INNER JOIN edgex_blob_available as b
+				ON a.resource_blob_id = b.id
+				WHERE a.resource_blob_id != ""
+			INTERSECT
+				SELECT
+					a.id
+				FROM metadata_runtime_entity_metadata as a
+				INNER JOIN edgex_blob_available as b
+				ON a.resource_blob_id = b.id
+				WHERE a.resource_blob_id != ""
+
+			UNION
+				SELECT
+					a.id
+				FROM metadata_runtime_entity_metadata as a
+				INNER JOIN edgex_blob_available as b
+				ON a.bean_blob_id = b.id
+				WHERE a.resource_blob_id = ""
+		)
+
 	;
 	`)
 
