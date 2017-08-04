3c15e9d6e0944bff11d11ba63b7e96820efb6e02
Haoming Zhang <zhm1993zhm@gmail.com>
2017-08-04 14:19:16 -0700

    [XAPID-1002] add validate iso8601 time

--- clients.go
+++ clients.go
@@ -14,7 +14,6 @@ const (
 	trackerConfigStatusEndpoint = "/serviceconfigstatus"
 	trackerHeartbeatEndpoint    = "/serviceheartbeat/{uuid}"
 	trackerRegisterEndpoint     = "/serviceregister/{uuid}"
-	ApigeeSyncTokenSelector     = "ApigeeSyncToken"
 )
 
 type trackerClientInterface interface {
